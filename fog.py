03eee095bec68b1f3a40a42fbfa671858b186cb4
gsmalves <gustavoeu2017@gmail.com>
2021-11-03 19:03:35 -0300

    up

--- fog.py
+++ fog.py
@@ -37,9 +37,9 @@ def connect_mqtt() -> paho.mqtt.client:
 def connect_broker() -> paho.mqtt.client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print("Connected to HiveMQ!")
+            print("Connecto ao HiveMQ!")
         else:
-            print("Failed to connect, return code %d\n", rc)
+            print("Falha na conexão, return code %d\n", rc)
 
     client = paho.mqtt.client.Client(client_id,clean_session=False)
     client.on_connect = on_connect
@@ -50,8 +50,10 @@ def connect_broker() -> paho.mqtt.client:
 def subscribe(client: paho.mqtt.client, client_broker: paho.mqtt.client):
     def on_message(client, userdata, message)->list:
         data = json.loads(str(message.payload.decode("utf-8")))
+        print(data)
         idSelect = requests.get(url=f'{heroku}/getId').json()
         n = requests.get(url=f'{heroku}/getN').json()
+        print()
         data['fog'] = os.getenv("FOG")
         index = next((i for i, item in enumerate(lista) if item['id'] == data['id']), -1) 
         if index != -1:
@@ -60,12 +62,19 @@ def subscribe(client: paho.mqtt.client, client_broker: paho.mqtt.client):
             lista.append(data)
         index = next((i for i, item in enumerate(lista) if item['id'] == idSelect), -1) 
         if index != -1:
-            client_broker.publish("MonitorarPaciente",str(lista[index]))
+            print("cheguei aqui")
+            resultado = client_broker.publish("MonitorarPaciente",str(lista[index]))
+            print(str(lista[index]))
+            status = resultado[0]
+            if status == 0:
+                print(f"Enviou de boa para topico de monitorar")
+            else:
+                print(f"Falhou ao enviar a mensagem ao topico de monitorar")
         ordenada = sorted(lista, key=lambda k: k['status'], reverse=True) 
         result = client_broker.publish(topic2, str(ordenada[0:n]))
         status = result[0]
         if status != 0:
-            print(f"Failed to send message to topic {topic2}")
+            print(f"Falhou ao enviar a mensagem ao topico {topic2}")
     client.subscribe(topic)
     client.on_message = on_message
 
